98a79da3bd9504d83b32e0424b4b78190ec07236
Ben Lambert <ben.lambert@blackbaud.com>
2018-12-13 16:12:57 -0500

    exported newly added types/enums

--- src/addin/client-interfaces/index.ts
+++ src/addin/client-interfaces/index.ts
@@ -1,3 +1,4 @@
+export * from './addin-button-style';
 export * from './addin-client-args';
 export * from './addin-client-callbacks';
 export * from './addin-client-close-modal-args';
@@ -5,5 +6,10 @@ export * from './addin-client-init-args';
 export * from './addin-client-navigate-args';
 export * from './addin-client-open-help-args';
 export * from './addin-client-ready-args';
+export * from './addin-client-ready-button-config';
+export * from './addin-client-ready-tab-config';
+export * from './addin-client-ready-tile-config';
 export * from './addin-client-show-modal-args';
 export * from './addin-client-show-modal-result';
+export * from './addin-tab-summary-style';
+export * from './addin-tile-summary-style';
