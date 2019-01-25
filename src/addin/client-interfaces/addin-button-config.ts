f70327ae12f7698ad3f7a41e03b0c90c1eed0859
Adam Hickey <adam.hickey@blackbaud.com>
2019-01-25 11:11:53 -0500

    Add buttons to callback args.

---
+++ src/addin/client-interfaces/addin-button-config.ts
@@ -0,0 +1,17 @@
+import { AddinButtonStyle } from './addin-button-style';
+
+/**
+ * Interface for defining configuration options for Button add-ins
+ */
+export interface AddinButtonConfig {
+
+  /**
+   * The style to use for the button
+   */
+  style?: AddinButtonStyle;
+
+  /**
+   * The text to use for the button
+   */
+  text?: string;
+}
